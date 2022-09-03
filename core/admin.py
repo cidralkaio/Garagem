6e49018dbb4c92de306fde2b2d759112097e519d
kaio <kaiorodriguescidral123@gmail.com>
2022-09-03 13:24:24 -0300

    GaragemTerminada

---
+++ core/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from core.models import Carro
+from core.models import Marca
+from core.models import Categoria
+
+admin.site.register(Carro)
+admin.site.register(Marca)
+admin.site.register(Categoria)
+
+# Register your models here.
---
+++ core/views.py
@@ -0,0 +1,19 @@
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Categoria, Marca, Carro
+from core.serializers import CategoriaSerializer, MarcaSerializer, CarroSerializer
+
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+
+class CarroViewSet(ModelViewSet):
+    queryset = Carro.objects.all()
+    serializer_class = CarroSerializer
+
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
